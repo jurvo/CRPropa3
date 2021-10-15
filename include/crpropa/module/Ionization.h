fe1355be6f79e9fbd217791247af7cc918699218
JulienDoerner <julien.doerner@rub.de>
2021-10-15 13:50:08 +0200

    add energy loss functions to ionization

--- include/crpropa/module/Ionization.h
+++ include/crpropa/module/Ionization.h
@@ -16,15 +16,19 @@ class Ionization: public Module {
 private:
     ref_ptr<Density> density;
     std::string propertyLabel = "ionizations";
+    double limit;
+    bool count;
+
     double IH = 13.598 * eV;
     double IH2 = 15.603 * eV;
     std::vector<double> anDiss = {-53.23133, 96.57789, -67.57069, 23.32707, -4.004618, 0.272652}; // parameter for polynomial fit
     std::vector<double> anDoub = {-125.8689, 172.0709, -108.8777, 34.18291, -5.358045, 0.335476}; // parameter for polynomial fit
+    double sigmaT = 2 / M_PI / 3 * pow_integer<2>(fine_structure * h_planck / mass_electron / c_light);
 
 public:
-    Ionization(ref_ptr<Density> density);
+    Ionization(ref_ptr<Density> density, double limit = 0.01, bool count = true);
 
-    void process(Candidate *cand);
+    void process(Candidate *cand) const;
 
 
 private: 
@@ -32,17 +36,18 @@ private:
         @param E: kinetic Energy of the incoming particle in [J]
         @return crossection for the process in [m^2]
     */
-    double sigma_p_ion(double E);
-    double sigma_p_ec(double E);
-    double sigma_p_diss(double E);
-    double sigma_p_doub(double E);
+    double sigma_p_ion(double E) const;
+    double sigma_p_ec(double E) const;
+    double sigma_p_diss(double E) const;
+    double sigma_p_doub(double E) const;
 
-    double sigma_e_ion(double E);
-    double sigma_e_diss(double E);
-    double sigma_e_doub(double E);
+    double sigma_e_ion(double E) const;
+    double sigma_e_diss(double E) const;
+    double sigma_e_doub(double E) const;
 
-    double energyLossRate(double E);
-    void countInteractionsOnStep(double E, double step, Candidate *cand);
+    double energyLossRateElectron(double E, Vector3d &pos) const;
+    double energyLossRateProton(double E, Vector3d &pos) const;
+    void countInteractionsOnStep(double E, double step, Candidate *cand) const;
 
 };
 
--- src/module/Ionization.cpp
+++ src/module/Ionization.cpp
@@ -1,16 +1,75 @@
 #include "crpropa/module/Ionization.h"
 #include "crpropa/Common.h"
+#include "crpropa/ParticleID.h"
+#include "crpropa/Random.h"
 
 using namespace crpropa;
 
-Ionization::Ionization(ref_ptr<Density> density) : 
-    density(density) {
+Ionization::Ionization(ref_ptr<Density> density, double limit, bool count) : 
+    density(density), limit(limit), count(count) {
 
 }
 
+void Ionization::process(Candidate *cand) const {
+    int id = abs(cand -> current.getId());
+    if ((id != nucleusId(1,1)) && (id != 11))
+        return; // only for electrons and for protons
+    bool electron = (id == 11);
+    double step = cand -> getNextStep();
+    double E = cand -> current.getEnergy();
+    Vector3d pos = cand -> current.getPosition();
+
+    double dE = 0;
+    if (electron){
+        dE = energyLossRateElectron(E, pos) * step;
+    }
+    else {
+        dE = energyLossRateProton(E, pos) * step;
+    }
+
+    cand -> current.setEnergy(E - dE);
+
+    if(not count)
+        return;
+
+    double totalCrossection;
+    if (electron) {
+        totalCrossection += sigma_e_ion(E); 
+        totalCrossection += sigma_e_diss(E);
+        totalCrossection += sigma_e_doub(E);
+    }
+    else {
+        totalCrossection += sigma_p_ion(E); 
+        totalCrossection += sigma_p_ec(E);
+        totalCrossection += sigma_p_diss(E);
+        totalCrossection += sigma_p_doub(E);
+    }
+
+    double dens = density -> getH2Density(pos) + density -> getHIDensity(pos);
+    double tau = totalCrossection * step * dens;
+    int nrItteration = 1;
+    while(tau > 0.01) {
+        step /= 2;
+        nrItteration *= 2;
+        tau = totalCrossection * step * dens;
+    }
+    
+    Random &random = Random::instance();
+    int nrCount = 0;
+    for (int i = 0; i < nrItteration; i++) {
+        if(random.rand() > tau){
+            nrCount++;
+        }
+    }
+
+    if (cand -> hasProperty(propertyLabel)) {
+        nrCount += cand -> getProperty(propertyLabel);
+    }
+    cand -> setProperty(propertyLabel, nrCount);
+}
 
 
-double Ionization::sigma_p_ion(double E) {
+double Ionization::sigma_p_ion(double E) const {
     // fiting parameter
     double A = 0.71; 
     double B = 1.63;
@@ -26,7 +85,7 @@ double Ionization::sigma_p_ion(double E) {
     return 1 / (1 / sigmaL + 1 / sigmaH);
 }
 
-double Ionization::sigma_p_ec(double E) {
+double Ionization::sigma_p_ec(double E) const {
     double A = 1.044; 
     double B = 2.88;
     double C = 0.016; 
@@ -41,15 +100,15 @@ double Ionization::sigma_p_ec(double E) {
     return sigma;
 }
 
-double Ionization::sigma_p_diss(double E) {
+double Ionization::sigma_p_diss(double E) const {
     return sigma_e_diss(mass_electron * E / mass_proton);
 }
 
-double Ionization::sigma_p_doub(double E) {
+double Ionization::sigma_p_doub(double E) const {
     return sigma_e_doub(mass_electron * E / mass_proton);
 }
 
-double Ionization::sigma_e_ion(double E) {
+double Ionization::sigma_e_ion(double E) const {
     double t = E / IH2;
     int N = 2; // number of electrons
 
@@ -65,7 +124,7 @@ double Ionization::sigma_e_ion(double E) {
     return sigma;
 }
 
-double Ionization::sigma_e_diss(double E) {
+double Ionization::sigma_e_diss(double E) const {
     if(E < 18.1 * eV) 
         return 0;
     
@@ -81,7 +140,7 @@ double Ionization::sigma_e_diss(double E) {
     return sigma;
 }
 
-double Ionization::sigma_e_doub(double E) {
+double Ionization::sigma_e_doub(double E) const {
     if(E < 51 * eV) 
         return 0;
 
@@ -95,4 +154,46 @@ double Ionization::sigma_e_doub(double E) {
 
     double sigma = exp(logSigma) * 1e-18 * cm * cm;
     return sigma;
+}
+
+double Ionization::energyLossRateElectron(double E, Vector3d &pos) const {
+    double mc2 = mass_electron * c_squared;
+    double gamma = E / mc2;
+    double nHI = density -> getHIDensity(pos);
+    double nH2 = density -> getH2Density(pos); 
+
+    // loss in neutral atomic matter 
+    // calculatet after Schlickeiser 2002 (eq 4.5.2)
+    double dGdT = nHI * (log(gamma) + 2. / 3. * log(mc2 / IH));
+    dGdT += nH2 * (log(gamma) + 2. / 3. * log(mc2 / IH2));
+    dGdT *= - 9. / 4. * c_light * sigmaT;
+    return dGdT * mc2;
+}
+
+double Ionization::energyLossRateProton(double E, Vector3d &pos) const {
+    double nHI = density -> getHIDensity(pos);
+    double nH2 = density -> getH2Density(pos);
+    double nHII = density -> getHIIDensity(pos);
+
+    double mc2 = mass_electron * c_squared; // rest energy electron
+    double Mc2 = mass_proton * c_squared; // rest energy proton
+    double gamma = E/Mc2;
+    double beta = sqrt(1 - 1 / gamma / gamma);
+    double beta2 = beta * beta;
+
+    // interactions in fully ionized plasma. (only proton)
+    // calculatet after Schlickeiser 2002 (eq. 5.3.34) 
+    // in the limit of relativistic protons and cold plasmas (W_e = 1)
+    double dEdT_ion = 30 * c_light * sigmaT * mc2 / beta * nHII;
+
+    // interactions in neutral matter
+    double T = E - Mc2;
+    double Qmax =  (T + 2 * Mc2) / (1 + pow_integer<2>((mass_proton + mass_electron)*c_light) / (2 * mass_electron * T) );
+    double BHI = log((2 * mc2 * beta2 * Qmax) / (pow_integer<2>(IH / gamma))) - 2 * beta2;
+    double BH2 = log((2 * mc2 * beta2 * Qmax) / (pow_integer<2>(IH2 / gamma))) - 2 * beta2;
+
+    double dEdT_ato = 3 * c_light * sigmaT * mc2 / 4 / beta;
+    dEdT_ato *= nHI * BHI + NH2 * BH2;
+
+    return dEdT_ion + dEdT_ato;
 }
