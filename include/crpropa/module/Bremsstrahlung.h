6dc84236744e2446ac089df6440fa3d61b9339d0
JulienDoerner <julien.doerner@rub.de>
2021-11-23 11:41:45 +0100

    clean up bremsstrahlung module
    remove unsued code.
    Apply only strong-shielding limit

--- include/crpropa/module/Bremsstrahlung.h
+++ include/crpropa/module/Bremsstrahlung.h
@@ -27,55 +27,44 @@ protected:
     double limit;   // limit next step length to limit the interaction probability to a given value
     bool havePhotons; // create secondary photons
     double secondaryThreshold; // threshold energy of the secondary photons
-    bool useDoubleIntegration; // calculate total crossection with two integrals instead of one
-    double maximalVariationFactor; // maximal variation of the density between this step and the next one
-    double fMin; // fraction of the minimum energy of the sampled photon
 
     double alpha; // fine structure constant
     double sigmaT; // thomson crossection
-    std::vector<double> tabDelta; // tabulated scattering functions 
-    std::vector<double> tabPhi1;
-    std::vector<double> tabPhi2;
-    double mc2 = mass_electron * c_squared;
+    double mc2 = mass_electron * c_squared; // rest energy of a electron
 
 public:
-    Bremsstrahlung(ref_ptr<Density> density, double limit= 0.01, double secondaryThreshold = 10 * keV);
+    Bremsstrahlung(
+        ref_ptr<Density> density, 
+        double limit= 0.01, 
+        double secondaryThreshold = 10 * keV
+    );
 
     void process(Candidate *candidate) const;
 
-    // set the density distribution
-    void setDensity(ref_ptr<Density> density);
-    // set the limit of interaction probability per step
-    void setLimit(double limit);
-    // if true secondaries are produced
-    void setHavePhotons(bool photons);
-    // lower threshold for the produced secondaries. Requires that setHavePhotons is set to true.
-    void setSecondaryThreshold(double threshold);
-    // calculate the total crossection with to integrals. (Default is true)
-    void setUseDoubleIntegration(bool use);
-    // allows a maximal variation of gas density for step length
-    void setMaximalVariationFactor(double factor);
-
-    void setFMin(double fraction);
-    double getFMin() const;
-
-    ref_ptr<Density> getDensity() const;
-    double getLimit() const;
-    bool getHavePhotons() const;
-    double getSecondaryThreshold() const;
-    bool getUseDoubleIntegration() const;
-    double getMaximumVariationFactor() const;
+    // differential crossection. Parametrized after Schlickeiser 2002.
+    double differentialCrossection(double E, double eps) const;
+    double getCrossection(double E) const;
 
     // calculate the produced photon energy. This is also used for the energy loss.
     double samplePhotonEnergy(double Ein) const;
-    // differential crossection. Parametrized after Schlickeiser 2002.
-    double sigmaH(double E, double Egamma) const;
-    // maximal energy of secondary photon.
-    double maximumEps(double E) const;
-    // photon energy of the break between tabulated scattering functions and unshielded approximation.
-    double epsBreak(double E) const;
 
-    double getCrossection(double E) const;
+    // set the density distribution
+    void setDensity(ref_ptr<Density> density);
+    ref_ptr<Density> getDensity() const;
+
+    // set the limit of interaction probability per step
+    void setLimit(double limit);
+    double getLimit() const;
+
+    // if true secondaries are produced
+    void setHavePhotons(bool photons);
+    bool getHavePhotons() const;
+
+    // lower threshold for the produced secondaries. Requires that setHavePhotons is set to true.
+    void setSecondaryThreshold(double threshold);
+    double getSecondaryThreshold() const;
+
+    // calculate density as n = n_HI + 2 * n_H2 
     double getDensityAtPosition(Vector3d &pos) const;
 };
 
--- src/module/Bremsstrahlung.cpp
+++ src/module/Bremsstrahlung.cpp
@@ -3,20 +3,15 @@
 #include "crpropa/Random.h"
 #include "crpropa/Common.h"
 
-
 #include <cmath>
 
 namespace crpropa
 {
 
 Bremsstrahlung::Bremsstrahlung(ref_ptr<Density> density, double limit, double threshold) : 
-    density(density), limit(limit), havePhotons(false), secondaryThreshold(threshold), useDoubleIntegration(true), maximalVariationFactor(0.1) {
-    tabDelta = {0., 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10};
-    tabPhi1 = {45.79, 45.43, 45.09, 44.11, 42.64, 40.16, 34.97, 29.97, 24.73, 18.09, 13.65};
-    tabPhi2 = {44.46, 44.38, 44.24, 43.65, 42.49, 40.19, 34.93, 29.78, 24.34, 17.28, 12.41};
+    density(density), limit(limit), havePhotons(false), secondaryThreshold(threshold) {
     sigmaT = 6.652458558e-29 * meter * meter;
     alpha = 1/137.037;
-    fMin = 1e-4;
 }
 
 void Bremsstrahlung::process(Candidate *cand) const {
@@ -27,19 +22,9 @@ void Bremsstrahlung::process(Candidate *cand) const {
     double step = cand -> getCurrentStep();
     double n0 = getDensityAtPosition(pos);
     double Ein = cand -> current.getEnergy();
-
-    // check for maximal variation of the density
-    Vector3d dir = cand -> current.getDirection();
-    Vector3d pos2 = pos + step*dir; 
-    double n1 = getDensityAtPosition(pos2);
-    if(abs(n1 - n0) / n0 > maximalVariationFactor) {
-        // ToDo:    limit step size and perform half step 
-        throw std::runtime_error("to big changes in density. Please take smaler step sizes. \n");
-    }
-
     double crossection = getCrossection(Ein);
 
-    if (crossection <= 0){
+    if (crossection < 0){
         throw std::runtime_error("no crossection left");
     }
 
@@ -60,18 +45,7 @@ void Bremsstrahlung::process(Candidate *cand) const {
 }
 
 double Bremsstrahlung::getCrossection(double Ein) const {
-    double crossection = 0.;
-    double epsMax = maximumEps(Ein);
-
-    if(useDoubleIntegration) {
-        double eps0 = epsBreak(Ein);
-        crossection += gaussInt([this, Ein](double eps) {return this -> sigmaH(Ein, eps); }, 0, eps0);
-        crossection += gaussInt([this, Ein](double eps) {return this -> sigmaH(Ein, eps); }, eps0 + 1e-5 * Ein, epsMax);
-    }
-    else{
-        crossection += gaussInt([this, Ein](double eps) {return this -> sigmaH(Ein, eps); }, 0, epsMax);
-    }
-    return crossection;
+    return gaussInt([this, Ein](double eps) {return this -> differentialCrossection(Ein, eps); }, 0, Ein);
 }
 
 void Bremsstrahlung::setDensity(ref_ptr<Density> density) {
@@ -90,14 +64,6 @@ void Bremsstrahlung::setSecondaryThreshold(double threshold) {
     this -> secondaryThreshold = threshold;
 }
 
-void Bremsstrahlung::setUseDoubleIntegration(bool use) {
-    this -> useDoubleIntegration = use;
-}
-
-void Bremsstrahlung::setMaximalVariationFactor(double factor) {
-    this -> maximalVariationFactor = factor;
-}
-
 ref_ptr<Density> Bremsstrahlung::getDensity() const{
     return this -> density;
 }
@@ -114,54 +80,30 @@ double Bremsstrahlung::getSecondaryThreshold() const {
     return this -> secondaryThreshold;
 }
 
-bool Bremsstrahlung::getUseDoubleIntegration() const {
-    return this -> useDoubleIntegration;
-}
-
-double Bremsstrahlung::getMaximumVariationFactor() const {
-    return this -> maximalVariationFactor;
-}
-
 double Bremsstrahlung::samplePhotonEnergy(double Ein) const {
     double epsMin = 0;
-    double epsMax = maximumEps(Ein); 
+    double epsMax = Ein; 
     Random &random = Random::instance();
 
     for (int i = 0; i < 1000000; i++)
     {
         double eps = epsMin + random.rand() * (epsMax - epsMin);
-        double pEps = sigmaH(Ein, eps);
-        if(pEps > random.rand() * sigmaT/eV){
+        double pEps = differentialCrossection(Ein, eps);
+        if(pEps > random.rand() * sigmaT * 1e-3){
             return eps;
         }
     }
     throw std::runtime_error("error in sampling: no photon found! \n");    
 }
 
-// parametrisation of crossection after Schlickeiser 2002
-double Bremsstrahlung::sigmaH(double E, double Egamma) const {
-    double Delta = Egamma * mc2 / 4 / alpha / E / (E - Egamma);
-    double Phi1, Phi2;
-    if (Delta < 2) {
-        Phi1 = interpolate(Delta, tabDelta, tabPhi1);
-        Phi2 = interpolate(Delta, tabDelta, tabPhi2);
-    }
-    else {
-        Phi1 = 4 * (log(2 * E / mc2 * (E - Egamma) / Egamma) - 1. / 2.);
-        Phi2 = Phi1;
-    }
-    double sigma = (1 + pow_integer<2>(1 - Egamma / E) ) * Phi1 - 2. / 3. * (1 - Egamma / E) * Phi2;
-    sigma *= 3. / 8. / M_PI * alpha * sigmaT;
-    return sigma / Egamma / eV;
+// parametrisation of crossection after Schlickeiser 2002 in the strong shielding limit
+double Bremsstrahlung::differentialCrossection(double E, double eps) const {
+    return 3 / (8 * M_PI * eps) * alpha * sigmaT * 45 * (4./3. - 4/3 * eps / E + pow_integer<2>(eps / E));
 }
 
-double Bremsstrahlung::maximumEps(double E) const {
-    return E / (mc2 / 2 / E * exp(0.5) + 1);
+double Bremsstrahlung::getDensityAtPosition(Vector3d &pos) const {
+    double n = density -> getHIDensity(pos) + 2 * density -> getH2Density(pos);
+    return n;
 }
 
-double Bremsstrahlung::epsBreak(double E) const {
-    return E / (mc2 / 8. / alpha / E + 1);
-}
-
-
-} // namespace crpropa
+} // namespace crpropa 
