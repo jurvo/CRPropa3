375de167acd79edf8a845f77340bb0d1cbcfc80a
JulienDörner <julien.doerner@rub.de>
2022-01-24 10:36:11 +0100

    add variable scale height for ModulatedTurbulentField

--- include/crpropa/magneticField/turbulentField/ModulatedTurbulentField.h
+++ include/crpropa/magneticField/turbulentField/ModulatedTurbulentField.h
@@ -14,16 +14,20 @@ private:
     ref_ptr<TurbulentField> field;
     double brmsField; // rms value of the input field. (will be normed to one)
     std::vector<double> radii; // measurement of radial position
-    std::vector<double> brms; // measurement of b_rms at radius
+    std::vector<double>  brms; // measurement of b_rms at radius
     double h; // norm values for exponential cut
+    std::vector<double> zHeight;
 public:
     ModulatedTurbulentField(ref_ptr<TurbulentField> field, std::string filePath, double h1);
-
+    ModulatedTurbulentField(ref_ptr<TurbulentField> fiedl, std::string filePath);
     void loadData(std::string filePath);
+    void loadData3D(std::string filePath);
     void setTurbulentField(ref_ptr<TurbulentField> field);
     Vector3d getField(const Vector3d &pos) const;
     //Vector3d getField(const Vector3d pos, double z) const;
     double getBrmsAtPosition(const Vector3d &pos) const;
+
+    void printData();
 };
 
 } // namespace
--- src/magneticField/turbulentField/ModulatedTurbulentField.cpp
+++ src/magneticField/turbulentField/ModulatedTurbulentField.cpp
@@ -49,10 +49,63 @@ Vector3d ModulatedTurbulentField::getField(const Vector3d &pos) const {
 
 double ModulatedTurbulentField::getBrmsAtPosition(const Vector3d &pos) const {
     double r = std::sqrt(pos.x * pos.x + pos.y * pos.y);
+    if(r > 15 * kpc){
+        return 0;
+    }
     double z = pos.z;
 
     double Br = interpolate(r, radii, brms);
-    return Br * std::exp(- fabs(z) / h);
+    double scaleHeight = 0;
+    if (h > 0)
+        scaleHeight = h;
+    else{
+        scaleHeight = interpolate(r, radii, zHeight);
+    }
+    if( scaleHeight != 0)
+        return Br * std::exp(- fabs(z) / scaleHeight);
+    else
+        return 0;
+}
+
+ModulatedTurbulentField::ModulatedTurbulentField(ref_ptr<TurbulentField> field, std::string filePath) {
+    setTurbulentField(field);
+    loadData3D(filePath);
+    h = -1;
+}
+
+void ModulatedTurbulentField::loadData3D(std::string filePath) {
+    std::ifstream infile(filePath.c_str());
+
+    if(!infile.good())
+        throw std::runtime_error("ModulatedTurbulentField::loadData could not open "+ filePath);
+
+    std::istream *in;
+    std::string line;
+    in = &infile;
+
+    double r, b, z;
+    while (std::getline(*in, line))
+    {
+        std::stringstream stream(line);
+        if (stream.peek() == '#')
+            continue;
+        
+        stream >> r >> b >> z;
+        radii.push_back(r * kpc);
+        brms.push_back(b * gauss);
+        zHeight.push_back(z * kpc);
+    }
+    infile.close();
+}
+
+
+void ModulatedTurbulentField::printData(){
+    for(int i = 0; i < radii.size(); i++){
+        std::cout << "i: "<< i << "\t"
+            << "r: " << radii[i]/kpc << "\t"
+            << "brms:" << brms[i]/gauss << "\t"
+            << "zH: " << zHeight[i] / kpc << "\n";
+    }
 }
 
 // Vector3d ModulatedTurbulentField::getField(const Vector3d &pos, double z) const {
