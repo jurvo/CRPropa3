54a95b0566c489d9250d8fe3a3725ccab67cded7
Jurek Völp <jurek.voelp@ruhr-uni-bochum.de>
2023-02-03 10:29:26 +0100

    added OneDimensionalDiffusionCoefficent minor changes in code structure

--- include/crpropa/module/DiffusionCoefficent.h
+++ include/crpropa/module/DiffusionCoefficent.h
@@ -14,68 +14,24 @@
 
 namespace crpropa {
 
-struct BTensor
-{
-	double parallel;
-	double perpendicular;
-
-	BTensor(double par, double perp);
-};
-
-
+#pragma region Base class DiffusionCoefficent
 
 /** DIFFUSIONCOEFFICENT
  @class DiffusionCoefficent
  @brief Abstract base class for a space dependet diffusion coefficent.
  */
 class DiffusionCoefficent: public Referenced {
-public:
-	virtual ~DiffusionCoefficent() {}
-	virtual double getCoefficent(const Vector3d &position) const { return 0; }
-	virtual BTensor getBTensor(double r) const { return BTensor(1.,1.);}
-	//virtual double getDivergence(const Vector3d &position) const = 0;
-	static const double D0; // Normalization diffusion coefficent, unit: m^2/s
-	static const double E0; // Energy normalization for the diffusion coefficent, unit: eV
-};
-
-
-/**
- @class DiffusionCoefficentList
- @brief Diffusion coefficent decorator implementing a superposition of fields.
- */
-/*
-class DiffusionCoefficentList: public DiffusionCoefficent {
-	std::vector<ref_ptr<DiffusionCoefficent> > coefficents;
-public:
-	void addCoefficent(ref_ptr<DiffusionCoefficent> coefficent);
-	Vector3d getCoefficent(const Vector3d &position) const;
-	double getDivergence(const Vector3d &position) const;
-};
-*/
-/**
- @class UniformDiffusionCoefficent
- @brief Spatial independent diffusion coefficent 
- */
-class UniformDiffusionCoefficent: public DiffusionCoefficent {
-//private:	
+protected:
 	double epsilon;
 	double scale;
 	double alpha;
 public:
-	/// @brief 
-	/// @param epsilon 
-	/// @param scale 
-	/// @param alpha 
-	UniformDiffusionCoefficent(double epsilon, double scale, double alpha);
-	double getCoefficent() const;
-	double getCoefficent(const Vector3d &position) const;
-
-	/// @brief Calculates the Diffusion Tensor for a particle
-	/// @param r Ridgidity of the particle
-	/// @return Diffuison Tensor parallel and perpendicular component
-	BTensor getBTensor(double r) const override;
-	//BTensor getBTensor(double r, Vector3d pos = , Vector3d dir = 0., double z = 0.) const;
-	//Vector3d getGradient(const Vector3d &position) const;
+	virtual ~DiffusionCoefficent() {}
+	virtual Vector3d getDiffusionCoefficent(double r, Vector3d pos) const = 0;
+	virtual Vector3d getDerivativeOfDiffusionCoefficent(double r, Vector3d pos) const = 0;
+	virtual Vector3d getBTensor(double r, Vector3d pos) const = 0;
+	static const double D0; // Normalization diffusion coefficent, unit: m^2/s
+	static const double E0; // Energy normalization for the diffusion coefficent, unit: eV
 
 	void setEpsilon(double e);
 	void setAlpha(double a);
@@ -85,8 +41,98 @@ public:
 	double getAlpha() const;
 	double getScale() const;
 
+	virtual std::string getDescription() const = 0;
+};
+#pragma endregion
+
+#pragma region UniformDiffusionCoefficent
+/**
+ @class UniformDiffusionCoefficent
+ @brief Spatial independent diffusion coefficent
+ */
+class UniformDiffusionCoefficent: public DiffusionCoefficent 
+{
+public:
+	/**
+	 * @brief Construct a new Uniform Diffusion Coefficent object
+	 * 
+	 * @param epsilon 
+	 * @param scale 
+	 * @param alpha 
+	 */
+	UniformDiffusionCoefficent(double epsilon, double scale, double alpha);
+
+	Vector3d getDiffusionCoefficent() const;
+	Vector3d getDiffusionCoefficent(double r, Vector3d pos) const override;
+
+	Vector3d getDerivativeOfDiffusionCoefficent(double r, Vector3d pos) const override;
+
+	/**
+	 * @brief Calculates the Diffusion Tensor for a particle
+	 * 
+	 * @param r Ridgidity of the particle 
+	 * @param pos Positon of the particle
+	 * @return Vector3d Diffuison Tensor parallel and perpendicular component
+	 */
+	Vector3d getBTensor(double r, Vector3d pos) const override;
+	
+	//getBTensor(double r, Vector3d pos = , Vector3d dir = 0., double z = 0.) const;
+
 	std::string getDescription() const;
 };
+#pragma endregion
+
+#pragma region OneDimensionalDiffusionCoefficent
+/**
+ * @class OneDimensionalDiffusionCoefficent
+ * @brief DiffusionCoefficent in x-direction with shock at x = 0 and width x_sh approximated by tanh() 
+		with variable compression ratio r_comp = D_up/D_down
+ */
+class OneDimensionalDiffusionCoefficent: public DiffusionCoefficent
+{
+private:
+	double compression_rate; //compression ratio of shock
+	double shock_width; //shock width
+
+public:
+	/**
+	 * @brief Construct a new One Dimensional Diffusion Coefficent object
+	 * 
+	 * @param epsilon Diffusion ratio, k_perp = epsilon * k_parallel, 0 <= epsilon <= 1
+	 * @param scale Scaler for the Diffuison coefficent
+	 * @param alpha Energy power index
+	 * @param comp Compression ratio of the shock. It gets squared for the diffusion coefficent profile, to presever v_adv**2 / diff across the shock!
+	 * @param x_sh Shock width
+	 */
+	OneDimensionalDiffusionCoefficent(double epsilon, double scale, double alpha, double comp, double x_sh);
+
+	Vector3d getDiffusionCoefficent(double r, double x) const;
+
+	/**
+	 * @brief Get the Diffusion Coefficent object
+	 * 
+	 * @param r Ridgidity
+	 * @param pos location of the particle
+	 * @return Vector3d Diffusion Coefficent
+	 */ 
+	Vector3d getDiffusionCoefficent(double r, Vector3d pos) const override;
+
+	Vector3d getDerivativeOfDiffusionCoefficent(double r, double x) const;
+
+	Vector3d getDerivativeOfDiffusionCoefficent(double r, Vector3d pos) const override;
+
+	Vector3d getBTensor(double r, Vector3d pos) const override;
+	
+	void setCompressionRate(double s);
+	void setShockwidth(double w);
+
+	double getCompressionRate() const;
+	double getShockwidth() const;
+
+	std::string getDescription() const;
+};
+#pragma endregion
+
 } // namespace crpropa
 
 #endif // CRPROPA_ADVECTIONFIELD_H
--- src/module/DiffusionCoefficent.cpp
+++ src/module/DiffusionCoefficent.cpp
@@ -4,59 +4,12 @@ namespace crpropa {
 
 // ---- DiffusionCoefficent ----
 #pragma region Static members
-
 const double DiffusionCoefficent::D0 = 6.1e24;
 const double DiffusionCoefficent::E0 = 4.0e9;
-
 #pragma endregion
 
-BTensor::BTensor(double par, double perp) : parallel(par), perpendicular(perp) { }
-
-#pragma region DiffusionCoefficentList
-/*
-void DiffusionCoefficentList::addCoefficent(ref_ptr<DiffusionCoefficent> coefficent) {
-	coefficents.push_back(coefficent);
-}
-
-Vector3d DiffusionCoefficentList::getCoefficent(const Vector3d &position) const {
-	Vector3d b(0.);
-	for (int i = 0; i < coefficents.size(); i++)
-		b += coefficents[i]->getCoefficent(position);
-	return b;
-}
-
-double DiffusionCoefficentList::getDivergence(const Vector3d &position) const {
-	double D=0.;
-	// Work on default values for divergence or an error handling
-	for (int i = 0; i < coefficents.size(); i++)
-		D += coefficents[i]->getDivergence(position);
-	return D;
-}
-*/
-#pragma endregion
-
-#pragma region UniformDiffusionCoefficent
-
-// Constructor
-UniformDiffusionCoefficent::UniformDiffusionCoefficent(double epsilon, double scale, double alpha) : epsilon(epsilon), scale(scale), alpha(alpha)  { }
-
-double UniformDiffusionCoefficent::getCoefficent() const 
-{
-	return scale * D0;
-}
-
-double UniformDiffusionCoefficent::getCoefficent(const Vector3d &position) const 
-{
-	return getCoefficent();
-}
-
-BTensor UniformDiffusionCoefficent::getBTensor(double r) const
-{
-	double d = scale * D0 * pow((std::abs(r) / E0), alpha);
-	return BTensor(pow(2  * d, 0.5), pow(2 * epsilon * d, 0.5));
-}
-
-void UniformDiffusionCoefficent::setEpsilon(double e)
+#pragma region DiffusionCoefficent
+void DiffusionCoefficent::setEpsilon(double e)
 {
 	if ((e > 1) or (e < 0))
 		throw std::runtime_error(
@@ -64,7 +17,7 @@ void UniformDiffusionCoefficent::setEpsilon(double e)
 	epsilon = e;
 }
 
-void UniformDiffusionCoefficent::setAlpha(double a) 
+void DiffusionCoefficent::setAlpha(double a) 
 {
 	if ((a > 2.) or (a < 0))
 		throw std::runtime_error(
@@ -72,7 +25,7 @@ void UniformDiffusionCoefficent::setAlpha(double a)
 	alpha = a;
 }
 
-void UniformDiffusionCoefficent::setScale(double s) 
+void DiffusionCoefficent::setScale(double s) 
 {
 	if (s < 0)
 		throw std::runtime_error(
@@ -80,34 +33,152 @@ void UniformDiffusionCoefficent::setScale(double s)
 	scale = s;
 }
 
-double UniformDiffusionCoefficent::getEpsilon() const
+double DiffusionCoefficent::getEpsilon() const
 {
 	return epsilon;
 }
 
-double UniformDiffusionCoefficent::getAlpha() const
+double DiffusionCoefficent::getAlpha() const
 {
 	return alpha;
 }
 
-double UniformDiffusionCoefficent::getScale() const
+double DiffusionCoefficent::getScale() const
 {
 	return scale;
+}
+#pragma endregion
+
+#pragma region UniformDiffusionCoefficent
+// Constructor
+UniformDiffusionCoefficent::UniformDiffusionCoefficent(double epsilon, double scale, double alpha)
+{ 
+	this->epsilon = epsilon;
+	this->scale = scale;
+	this->alpha = alpha;
+}
+
+Vector3d UniformDiffusionCoefficent::getDiffusionCoefficent() const 
+{
+	return Vector3d(scale * D0, epsilon * scale * D0, epsilon * scale * D0);
+}
+
+Vector3d UniformDiffusionCoefficent::getDiffusionCoefficent(double r, Vector3d pos) const 
+{
+	return getDiffusionCoefficent();
+}
+
+Vector3d UniformDiffusionCoefficent::getDerivativeOfDiffusionCoefficent(double r, Vector3d pos) const 
+{
+	return Vector3d(0.);
+}
+
+Vector3d UniformDiffusionCoefficent::getBTensor(double r, Vector3d pos) const
+{
+	double d = scale * D0 * pow((std::abs(r) / E0), alpha);
+	double k_perp = pow(2 * epsilon * d, 0.5);
+	return Vector3d(pow(2  * d, 0.5), k_perp, k_perp);
 }
 
 std::string UniformDiffusionCoefficent::getDescription() const 
 {
 	std::stringstream s;
 	s << "UniformDiffusionCoefficent\n";
-	s << "D: " << getCoefficent()  << " m^2/s\n";
+	s << "D: " << getDiffusionCoefficent()  << " m^2/s\n";
+	s << "scale: " << getScale() << "\n";
+	s << "epsilon: " << getEpsilon() << "\n";
+	s << "alpha: " << getAlpha();
+	return s.str();
+}
+#pragma endregion
+
+#pragma region OneDimensionalDiffusionCoefficent
+// Constructor
+OneDimensionalDiffusionCoefficent::OneDimensionalDiffusionCoefficent(double epsilon, double scale, double alpha, double comp, double x_sh) :
+																	compression_rate(comp), shock_width(x_sh)
+{ 
+	this->epsilon = epsilon;
+	this->scale = scale;
+	this->alpha = alpha;
+}
+
+Vector3d OneDimensionalDiffusionCoefficent::getDiffusionCoefficent(double r, double x) const
+{
+	double d_up = 1.;
+	double d_down = d_up/compression_rate;
+
+	double a = (d_up + d_down)*0.5;
+	double b = (d_up - d_down)*0.5;
+
+	double d = pow(a - b*tanh(x/shock_width), 2) * scale * D0 * pow((std::abs(r) / E0), alpha);
+
+	return Vector3d(d, epsilon * d, epsilon * d);
+}
+
+Vector3d OneDimensionalDiffusionCoefficent::getDiffusionCoefficent(double r, Vector3d pos) const 
+{
+	return getDiffusionCoefficent(r, pos.x);
+}
+
+Vector3d OneDimensionalDiffusionCoefficent::getDerivativeOfDiffusionCoefficent(double r, double x) const 
+{
+	double d_up = 1.;
+	double d_down = d_up/compression_rate;
+
+	double a = (d_up + d_down)*0.5;
+	double b = (d_up - d_down)*0.5;
+
+	double d = -2 * b * pow(1 / cosh(x / shock_width), 2) * (a - b*tanh(x/shock_width)) / shock_width * scale * D0 * pow((std::abs(r) / E0), alpha);
+
+	return Vector3d(d, 0., 0.);
+}
+
+Vector3d OneDimensionalDiffusionCoefficent::getDerivativeOfDiffusionCoefficent(double r, Vector3d pos) const 
+{
+	return getDerivativeOfDiffusionCoefficent(r, pos.x);
+}
+
+Vector3d OneDimensionalDiffusionCoefficent::getBTensor(double r, Vector3d pos) const
+{
+	Vector3d d = getDiffusionCoefficent(r, pos);
+	double b_par = pow(2  * d.x, 0.5);
+	double b_perp = pow(2  * d.y, 0.5);
+	return Vector3d(b_par, b_perp, b_perp);
+}
+
+std::string OneDimensionalDiffusionCoefficent::getDescription() const 
+{
+	std::stringstream s;
+	s << "OneDimensionalDiffusionCoefficent\n";
 	s << "scale: " << getScale() << "\n";
 	s << "epsilon: " << getEpsilon() << "\n";
 	s << "alpha: " << getAlpha();
 	return s.str();
 }
 
+void OneDimensionalDiffusionCoefficent::setCompressionRate(double s)
+{
+	compression_rate = s;
+}
+
+void OneDimensionalDiffusionCoefficent::setShockwidth(double w)
+{
+	shock_width = w;
+}
+
+double OneDimensionalDiffusionCoefficent::getCompressionRate() const
+{
+	return compression_rate;
+}
+
+double OneDimensionalDiffusionCoefficent::getShockwidth() const
+{
+	return shock_width;
+}
 
 #pragma endregion
+
+#pragma region Archive 
 /*
 // pos, dir, z not needed for now, but maybe later?
 void UniformDiffusionCoefficent::getBTensor(double r, double BTen[], Vector3d pos, Vector3d dir, double z) const 
@@ -119,13 +190,7 @@ void UniformDiffusionCoefficent::getBTensor(double r, double BTen[], Vector3d po
     return;
 }
 */
-
-/*Vector3d UniformDiffusionCoefficent::Vector3d getGradient(const Vector3d &position) const 
-{
-	return 0.;
-}*/
-
-
+#pragma endregion
 
 // ---- tbc ----
 } // namespace crpropa
